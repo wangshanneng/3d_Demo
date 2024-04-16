21d850d6266002146c00c6f44c01298750cffebe
小王匠 <wsncoder@163.com>
2024-04-16 23:06:15 +0800

    feat：新增Raycaster实现3d交互效果

--- src/main/main.js
+++ src/main/main.js
@@ -1,1 +1,100 @@
-console.log("Hello World");
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import gsap from "gsap";
+import * as dat from "dat.gui";
+
+const gui = new dat.GUI();
+
+// Demo：raycaster
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  300
+);
+
+const textureLoader = new THREE.TextureLoader();
+const particlesTexture = textureLoader.load("./textures/particles/1.png");
+// 设置相机位置
+camera.position.set(0, 0, 20);
+scene.add(camera);
+
+const cubeGeometry = new THREE.BoxBufferGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  wireframe: true,
+});
+const redMaterial = new THREE.MeshBasicMaterial({
+  color: "#ff0000",
+});
+
+// 1000立方体
+let cubeArr = [];
+for (let i = -5; i < 5; i++) {
+  for (let j = -5; j < 5; j++) {
+    for (let z = -5; z < 5; z++) {
+      const cube = new THREE.Mesh(cubeGeometry, material);
+      cube.position.set(i, j, z);
+      scene.add(cube);
+      cubeArr.push(cube);
+    }
+  }
+}
+
+// 创建投射光线对象
+const raycaster = new THREE.Raycaster();
+
+// 鼠标的位置对象
+const mouse = new THREE.Vector2();
+
+// 监听鼠标的位置
+window.addEventListener("click", (event) => {
+  //   console.log(event);
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -((event.clientY / window.innerHeight) * 2 - 1);
+  raycaster.setFromCamera(mouse, camera);
+  let result = raycaster.intersectObjects(cubeArr);
+  //   console.log(result);
+  //   result[0].object.material = redMaterial;
+  result.forEach((item) => {
+    item.object.material = redMaterial;
+  });
+});
+
+// 初始化渲染器
+const renderer = new THREE.WebGLRenderer();
+// 设置渲染的尺寸大小
+renderer.setSize(window.innerWidth, window.innerHeight);
+// 开启场景中的阴影贴图
+renderer.shadowMap.enabled = true;
+renderer.physicallyCorrectLights = true;
+
+document.body.appendChild(renderer.domElement);
+
+// 创建轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+// 设置控制器阻尼，让控制器更有真实效果,必须在动画循环里调用.update()。
+controls.enableDamping = true;
+
+// 添加坐标轴辅助器
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+// 设置时钟
+const clock = new THREE.Clock();
+
+function render() {
+  let time = clock.getElapsedTime();
+  controls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+
+render();
+
+// 监听画面变化，更新渲染画面
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+});
